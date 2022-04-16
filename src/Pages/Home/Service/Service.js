388f1bf7a63a093460aa426b3b89d71364c7b964
Md-Tur <turibnmostafiz@gmail.com>
2022-04-17 05:42:23 +0930

    destructuring added

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Service = ({ service }) => {
+    const { name, img, description, price } = service;
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Service;
