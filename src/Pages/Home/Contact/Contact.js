6a5bb70418118648624ee70cd2ac9986ff636a52
Md-Tur <turibnmostafiz@gmail.com>
2022-04-18 17:20:10 +0600

    contact section added

---
+++ src/Pages/Home/Contact/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className='container mt-3 mb-2 bg-success p-2 text-dark bg-opacity-25'>
+            <h2 className='text-primary text-center'>Contact Us</h2>
+            <div className='text-center'>
+                <p>Phone: 01218111999</p>
+                <p>Mail: dentalsolution@mail.com</p>
+                <p>Telephone: 21345678</p>
+                <p>Time: Every Saturday to Thursday (10am to 4pm)</p>
+                <p>Address: Bosundhara,Dhaka-1200</p>
+            </div>
+        </div>
+    );
+};
+
+export default Contact;
